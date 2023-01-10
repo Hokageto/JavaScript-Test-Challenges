679ce379b048b7d7fa6ad8cced045fce0d162554
Hokageto <didorhristozov@gmail.com>
2023-01-10 17:49:14 +0200

    JavaScript program that tracks the remaining days to Christmas

---
+++ js-coding-challenge-1/solution.js
@@ -0,0 +1,15 @@
+let today = new Date();
+
+let christmas = new Date(today.getFullYear(), 11, 25);
+
+if (today.getMonth() == 11 && today.getDate() > 25);
+{
+  christmas.getFullYear(christmas.getFullYear() + 1);
+}
+
+let dayDuration = 1000 * 60 * 60 * 24;
+
+console.log(
+  Math.ceil((christmas.getTime() - today.getTime()) / dayDuration) +
+    " days left until Christmas!"
+);
