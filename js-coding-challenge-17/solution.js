20502919d3fd3c25887bebd6cd5599d3bb8eb9e0
Hokageto <didorhristozov@gmail.com>
2023-01-10 19:47:51 +0200

    JavaScript program that checks if a given point lies strictly in the center of a given circle

---
+++ js-coding-challenge-17/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Number} a Point inside the circle
+ * @param {Number} b Point inside the circle
+ * @param {Number} x Center of the circle
+ * @param {Number} y Center of the circle
+ * @param {Number} r Radius
+ */
+function checkIfPointIsInsideCircle(a, b, x, y, r) {
+  const distance = Math.sqrt((x - a) ** 2 + (y - b) ** 2);
+  if (distance < r) {
+    return true;
+  }
+  return false;
+}
+
+console.log(checkIfPointIsInsideCircle(0, 0, 2, 4, 6));
+console.log(checkIfPointIsInsideCircle(0, 0, 6, 8, 6));
