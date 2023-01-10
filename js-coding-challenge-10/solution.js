9eeea5c0e583550b3f42e3aa03dc8b36718a350c
Hokageto <didorhristozov@gmail.com>
2023-01-10 18:15:31 +0200

    JavaScript program that removes the first character from two strings and concatinates them

---
+++ js-coding-challenge-10/solution.js
@@ -0,0 +1,7 @@
+function concatenate(str1, str2) {
+  return str1.slice(1) + str2.slice(1);
+}
+
+console.log(concatenate("PHP", "JS"));
+console.log(concatenate("A", "B"));
+console.log(concatenate("AA", "BB"));
