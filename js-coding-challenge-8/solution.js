95c8bad068871049a56cf8e99506e047ff89f0fe
Hokageto <didorhristozov@gmail.com>
2023-01-10 18:12:02 +0200

    Javascript program that reverses a given string.

---
+++ js-coding-challenge-8/solution.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("I like DogeCoin!"));
+console.log(reverseString("RaceCar"));
+console.log(reverseString("JavaScript"));
