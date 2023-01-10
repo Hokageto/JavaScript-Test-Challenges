98e50c3c4ede9d09993b25ac9d1dcc25c53e0dbf
Hokageto <didorhristozov@gmail.com>
2023-01-10 17:58:14 +0200

    JavaScript program that checks if a string starts with Java, reurn false if not

---
+++ js-coding-challenge-4/solution.js
@@ -0,0 +1,7 @@
+function checkStringStart(str) {
+  return str.startsWith("Java");
+}
+
+console.log(checkStringStart("JavaScript"));
+console.log(checkStringStart("Java"));
+console.log(checkStringStart("Python"));
