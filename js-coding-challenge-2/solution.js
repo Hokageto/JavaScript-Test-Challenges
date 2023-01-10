33295a7d35f12fa071e4e1f788f425a17f750091
Hokageto <didorhristozov@gmail.com>
2023-01-10 16:55:07 +0200

    commiting all of my JS challenges

--- js-coding-challenge-2/solution.js
+++ js-coding-challenge-2/solution.js
@@ -2,23 +2,13 @@
 ////////////////////////
 
 // function fToC(fahrenheit) {
-//   let fTemp = fahrenheit;
-//   let fToCel = ((fTemp - 32) * 5) / 9;
-//   let message = fTemp + "fahrenheit is " + fToCel + "celsius";
-//   console.log(message);
-// }
+//  }
 // fToC(100);
 
 // Celsius to Fahrenheit
 ////////////////////////
 
 function cToF(celsius) {
-  let cTemp = celsius;
-
-  let cToFahr = (cTemp * 9) / 5 + 32;
-
-  let message = cTemp + " celsius is " + cToFahr + " fahrenheit";
-
   console.log(message);
 }
 cToF(1);
