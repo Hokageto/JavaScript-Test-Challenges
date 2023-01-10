91f42d18c08a82e77cbaa56c61831fcc45271de2
Hokageto <didorhristozov@gmail.com>
2023-01-10 17:10:56 +0200

    commiting all of my JS challenges

--- js-coding-challenge-12/solution.js
+++ js-coding-challenge-12/solution.js
@@ -1,10 +1,4 @@
-function swap(arrayOfNumbers) {
-  [arrayOfNumbers[0], arrayOfNumbers[arrayOfNumbers.length - 1]] = [
-    arrayOfNumbers[arrayOfNumbers.length - 1],
-    arrayOfNumbers[0],
-  ];
-  return arrayOfNumbers;
-}
+function swap(arrayOfNumbers) {}
 console.log(swap([1, 2, 3, 4]));
 console.log(swap([0, 2, 1]));
 console.log(swap([3]));
