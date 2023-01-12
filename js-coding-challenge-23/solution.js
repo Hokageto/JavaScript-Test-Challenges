01fbf102fe608852603d99d107a36579be3ea400
Hokageto <didorhristozov@gmail.com>
2023-01-12 19:47:30 +0200

    JavaScript program that sorts an array of objects by their given ID

---
+++ js-coding-challenge-23/solution.js
@@ -0,0 +1,22 @@
+const library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    libraryID: 1254,
+  },
+  {
+    title: "Steve Jobs",
+    author: "Walter Isaacson",
+    libraryID: 4264,
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    libraryID: 3245,
+  },
+];
+
+library.sort(function (a, b) {
+  return a.libraryID - b.libraryID;
+});
+console.log(library);
